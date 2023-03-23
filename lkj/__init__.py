e85a7f94b12ec6f23dbe91f8b1ba645dfc997e67
Thor Whalen <thorwhalen1@gmail.com>
2023-03-23 09:41:41 +0100

    feat: get_app_data_folder

--- lkj/__init__.py
+++ lkj/__init__.py
@@ -1,4 +1,6 @@
-"""Lightweight Kit Jumpstart"""
+"""
+Lightweight Kit Jumpstart
+"""
 
 
 def get_caller_package_name(default=None):
@@ -15,3 +17,38 @@ def get_caller_package_name(default=None):
     except Exception as error:
         return default
 
+
+def get_app_data_folder():
+    """
+    Returns the full path of a directory suitable for storing application-specific data.
+
+    On Windows, this is typically %APPDATA%.
+    On macOS, this is typically ~/.config.
+    On Linux, this is typically ~/.config.
+
+    Returns:
+        str: The full path of the app data folder.
+
+    See https://github.com/i2mint/i2mint/issues/1.
+
+    >>> import os
+    >>> app_data_folder = get_app_data_folder()
+    >>> if os.name == 'nt': # Windows
+    ...     assert app_data_folder == os.getenv('APPDATA')
+    ... else: # macOS or linux/unix
+    ...     assert app_data_folder == os.path.expanduser('~/.config')
+
+    """
+    import os
+
+    if os.name == "nt":
+        # Windows
+        app_data_folder = os.getenv("APPDATA")
+    elif os.name == "darwin":
+        # macOS
+        app_data_folder = os.path.expanduser("~/.config")
+    else:
+        # Linux/Unix
+        app_data_folder = os.path.expanduser("~/.config")
+
+    return app_data_folder
